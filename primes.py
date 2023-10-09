78d395e0e30ad1f2941eed6a9a5cce76541fec96
Federico Schuscheim <federico.schuscheim@kcl.ac.uk>
2023-10-09 01:39:09 +0100

    Implement primes function

--- primes.py
+++ primes.py
@@ -2,5 +2,24 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("You must enter an integer number greater than 0")
+
     list = []
+    prime_counter = 0
+    trial_number = 2 # initialize it to first prime number
+
+    while prime_counter < number_of_primes:
+        prime = True
+        if len(list) != 0:
+            for prime_number in list:
+                if trial_number % prime_number == 0:
+                    prime = False
+        
+        if prime == True:
+            list.append(trial_number)
+            prime_counter += 1
+        
+        trial_number += 1
+    
     return list
